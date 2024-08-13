d266813f2246a71538a16f8936e2f44e07eff981
kolya <kolya@DESKTOP-3B548LN>
2024-08-13 04:39:09 +0400

    Добавьте файлы проекта.

---
+++ EyesBeta1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace EyesBeta1
+{
+    partial class EyeSelector
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EyeSelectResultLabel = new System.Windows.Forms.Label();
+            this.EyeSelectorButton = new System.Windows.Forms.Button();
+            this.DoubleEyeDiceButton = new System.Windows.Forms.Button();
+            this.DoubleEyeLabel = new System.Windows.Forms.Label();
+            this.EyeCountSelectCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // EyeSelectResultLabel
+            // 
+            this.EyeSelectResultLabel.AutoSize = true;
+            this.EyeSelectResultLabel.Location = new System.Drawing.Point(228, 123);
+            this.EyeSelectResultLabel.Name = "EyeSelectResultLabel";
+            this.EyeSelectResultLabel.Size = new System.Drawing.Size(16, 13);
+            this.EyeSelectResultLabel.TabIndex = 0;
+            this.EyeSelectResultLabel.Text = "...";
+            // 
+            // EyeSelectorButton
+            // 
+            this.EyeSelectorButton.Location = new System.Drawing.Point(41, 118);
+            this.EyeSelectorButton.Name = "EyeSelectorButton";
+            this.EyeSelectorButton.Size = new System.Drawing.Size(122, 23);
+            this.EyeSelectorButton.TabIndex = 2;
+            this.EyeSelectorButton.Text = "Выбрать глаз(а)";
+            this.EyeSelectorButton.UseVisualStyleBackColor = true;
+            this.EyeSelectorButton.Click += new System.EventHandler(this.EyeSelectorButton_Click);
+            // 
+            // DoubleEyeDiceButton
+            // 
+            this.DoubleEyeDiceButton.Location = new System.Drawing.Point(362, 118);
+            this.DoubleEyeDiceButton.Name = "DoubleEyeDiceButton";
+            this.DoubleEyeDiceButton.Size = new System.Drawing.Size(127, 23);
+            this.DoubleEyeDiceButton.TabIndex = 3;
+            this.DoubleEyeDiceButton.Text = "Куб на двойной глаз";
+            this.DoubleEyeDiceButton.UseVisualStyleBackColor = true;
+            this.DoubleEyeDiceButton.Click += new System.EventHandler(this.DoubleEyeDiceButton_Click);
+            // 
+            // DoubleEyeLabel
+            // 
+            this.DoubleEyeLabel.AutoSize = true;
+            this.DoubleEyeLabel.Location = new System.Drawing.Point(359, 200);
+            this.DoubleEyeLabel.Name = "DoubleEyeLabel";
+            this.DoubleEyeLabel.Size = new System.Drawing.Size(84, 13);
+            this.DoubleEyeLabel.TabIndex = 4;
+            this.DoubleEyeLabel.Text = "Двойной глаз: ";
+            // 
+            // EyeCountSelectCheckBox
+            // 
+            this.EyeCountSelectCheckBox.AutoSize = true;
+            this.EyeCountSelectCheckBox.Location = new System.Drawing.Point(41, 200);
+            this.EyeCountSelectCheckBox.Name = "EyeCountSelectCheckBox";
+            this.EyeCountSelectCheckBox.Size = new System.Drawing.Size(155, 17);
+            this.EyeCountSelectCheckBox.TabIndex = 5;
+            this.EyeCountSelectCheckBox.Text = "Выбрать сразу два глаза";
+            this.EyeCountSelectCheckBox.UseVisualStyleBackColor = true;
+            this.EyeCountSelectCheckBox.Visible = false;
+            this.EyeCountSelectCheckBox.CheckStateChanged += new System.EventHandler(this.EyeCountSelectCheckBox_CheckStateChanged);
+            // 
+            // EyeSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 264);
+            this.Controls.Add(this.EyeCountSelectCheckBox);
+            this.Controls.Add(this.DoubleEyeLabel);
+            this.Controls.Add(this.DoubleEyeDiceButton);
+            this.Controls.Add(this.EyeSelectorButton);
+            this.Controls.Add(this.EyeSelectResultLabel);
+            this.Name = "EyeSelector";
+            this.Text = "Глазобор 9000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EyeSelectResultLabel;
+        private System.Windows.Forms.Button EyeSelectorButton;
+        private System.Windows.Forms.Button DoubleEyeDiceButton;
+        private System.Windows.Forms.Label DoubleEyeLabel;
+        private System.Windows.Forms.CheckBox EyeCountSelectCheckBox;
+    }
+}
+
---
+++ EyesBeta1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace EyesBeta1
+{
+    public partial class EyeSelector : Form
+    {
+        public EyeSelector()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, string> EyePool = new Dictionary<string, string>()
+            {
+                { "Огонь", "D" },
+                { "Вода", "D" },
+                { "Воздух", "D" },
+                { "Земля", "D" },
+                { "Электро", "D" },
+                { "Растения", "D" },
+                { "Лёд", "D" },
+                { "Иллюзии", "C" },
+                { "Животные", "C" },
+                { "Зеркала", "C" },
+                { "Зелья", "C" },
+                { "Тени", "B" },
+                { "Яд", "B" },
+                { "Телепорт", "B" },
+                { "Сны", "B" },
+                { "Телепатия", "A" },
+                { "Гипноз", "А" },
+                { "Разум", "А" },
+            };
+
+        private string[] RandomEye()
+        {
+            Random random = new Random();
+            int buffer = random.Next(0, EyePool.Count);
+            string[] CurrEye = { EyePool.ElementAt(buffer).Key, EyePool.ElementAt(buffer).Value };
+            
+            int d2 = random.Next(0, 1);
+
+            while (d2 > 0)
+            {
+
+            }
+
+            return CurrEye;
+        }
+
+        private void EyeSelectorButton_Click(object sender, EventArgs e)
+        {
+            string[] Eye = RandomEye();
+
+            if (DoubleEyeSelected)
+            {
+                EyeSelectResultLabel.Text = Eye[0] + " " + RandomEye()[0];
+            }
+            else
+            {
+                EyeSelectResultLabel.Text = Eye[0]; 
+            }
+        }
+
+        private void DoubleEyeDiceButton_Click(object sender, EventArgs e)
+        {
+            string[] DoubleEyePoolChoice = {"Да", "Нет", "Нет", "Нет", "Нет"};
+            Random random = new Random();
+            int DoubleEye = random.Next(0, DoubleEyePoolChoice.Length);
+            string StandartEyeLabelText = "Двойной глаз: ";
+
+            DoubleEyeLabel.Text = StandartEyeLabelText + DoubleEyePoolChoice[DoubleEye];
+            
+        }
+
+        public bool DoubleEyeSelected = false;
+
+        private void EyeCountSelectCheckBox_CheckStateChanged(object sender, EventArgs e)
+        {
+            /*if (EyeCountSelectCheckBox.Checked)
+            {
+                DoubleEyeSelected = true;
+            }
+            else
+            {
+                DoubleEyeSelected = false;
+            }*/
+
+        }
+    }
+}
